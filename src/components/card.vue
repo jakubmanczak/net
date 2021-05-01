6d0e4c4d33c9d994abfa77e15c16dc8a7bf82d9a
jakubmanczak <j4kubmanczak@gmail.com>
2021-05-01 23:00:21 +0200

    :sparkles: covert SASS to SCSS + border radius on card change

--- src/components/card.vue
+++ src/components/card.vue
@@ -1,74 +1,93 @@
 <template>
-  <router-link v-if="href" :to="href" tag="div" class="card linkyes">
-    <img alt="" v-if="photo" :src="require(`../assets/card/${photo}`)" />
-    <div class="card-inner">
-      <p class="title" v-if="title">{{ title }}</p>
-      <p class="desc" v-if="desc">{{ desc }}</p>
-      <p class="meta" v-if="meta">{{ meta }}</p>
-    </div>
-  </router-link>
-  <div v-if="!href" class="card">
-    <img alt="" v-if="photo" :src="require(`../assets/card/${photo}`)" />
-    <div class="card-inner">
-      <p class="title" v-if="title">{{ title }}</p>
-      <p class="desc" v-if="desc">{{ desc }}</p>
-      <p class="meta" v-if="meta">{{ meta }}</p>
-    </div>
-  </div>
+	<router-link v-if="href" :to="href" tag="div" class="card linkyes">
+		<img alt="" v-if="photo" :src="require(`../assets/card/${photo}`)" />
+		<div class="card-inner">
+			<p class="title" v-if="title">{{ title }}</p>
+			<p class="desc" v-if="desc">{{ desc }}</p>
+			<p class="meta" v-if="meta">{{ meta }}</p>
+		</div>
+	</router-link>
+	<div v-if="!href" class="card">
+		<img alt="" v-if="photo" :src="require(`../assets/card/${photo}`)" />
+		<div class="card-inner">
+			<p class="title" v-if="title">{{ title }}</p>
+			<p class="desc" v-if="desc">{{ desc }}</p>
+			<p class="meta" v-if="meta">{{ meta }}</p>
+		</div>
+	</div>
 </template>
 
 <script lang="ts">
-  import { defineComponent } from 'vue';
+	import { defineComponent } from 'vue';
 
-  export default defineComponent({
-    name: 'card',
-    props: {
-      title: String,
-      photo: String,
-      desc: String,
-      meta: String,
-      href: String,
-    },
-  });
+	export default defineComponent({
+		name: 'card',
+		props: {
+			title: String,
+			photo: String,
+			desc: String,
+			meta: String,
+			href: String,
+		},
+	});
 </script>
 
-<style lang="sass" scoped>
-  .card
-    background-color: var(--bg-two)
-    display: flex
-    margin: 1em
-    border-radius: 8px
-    width: 24em
-    color: inherit
-    text-decoration: inherit
-    transition-duration: var(--trandur)
-    img
-      height: 86px
-      border-radius: 8px 0 0 8px
-    .card-inner
-      padding: .1em
-      margin-left: .5em
-      .title
-        font-size: 1.16em
-        font-weight: 600
-        letter-spacing: .1em
-        margin-left: .8em
-      .desc,.meta
-        font-size: .8em
-      .desc
-        text-align: justify
-      .meta
-        opacity: .5
-    &:focus,&:hover
-      &.linkyes
-        background-color: var(--bg-three)
-        transform: scale(1.025)
-        box-shadow: 0 0 12px black
-
-  @media only screen and (max-width: 872px)
-    .card
-      transform: scale(.75) translate(-15%)
-      &:focus,&:hover
-        &.linkyes
-          transform: scale(.8) translate(-15%)
+<style lang="scss" scoped>
+	.card {
+		background-color: var(--bg-two);
+		display: flex;
+		margin: 1em;
+		border-radius: 8px;
+		width: 24em;
+		color: inherit;
+		text-decoration: inherit;
+		transition-duration: var(--trandur);
+		img {
+			height: 86px;
+			border-radius: 8px 0 0 8px;
+		}
+		.card-inner {
+			padding: 0.1em;
+			margin-left: 0.5em;
+			.title {
+				font-size: 1.16em;
+				font-weight: 600;
+				letter-spacing: 0.1em;
+				margin-left: 0.8em;
+			}
+			.desc,
+			.meta {
+				font-size: 0.8em;
+			}
+			.desc {
+				text-align: justify;
+			}
+			.meta {
+				opacity: 0.5;
+			}
+		}
+		&:focus,
+		&:hover {
+			&.linkyes {
+				background-color: var(--bg-three);
+				transform: scale(1.025);
+				box-shadow: 0 0 12px black;
+				border-radius: 4px;
+				img {
+					border-radius: 4px;
+				}
+			}
+		}
+	}
+	@media only screen and (max-width: 872px) {
+		.card {
+			transform: scale(0.75) translate(-15%);
+			&:focus,
+			&:hover {
+				&.linkyes {
+					transform: scale(0.8) translate(-15%);
+				}
+			}
+		}
+	}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,233 +1,260 @@
 <template>
-  <nav>
-    <!-- <router-link to="/">:root</router-link> -->
-    <a href="#my-projects">My Projects</a>
-    <a href="#internet-presence">My Internet Presence</a>
-    <a href="#other">Other</a>
-  </nav>
-  <main class="mainContainer">
-    <div class="mainChild short">
-      <h1>Jakub Mańczak</h1>
-      <p>
-        Hi there! 👋 <br />
-        <br />
-        My name is Jakub Mańczak and I'm a high school student learning IT from
-        near Poznań, Poland.
-      </p>
-    </div>
-    <div class="mainChild">
-      <img src="@/assets/jakub3.png" alt="" />
-      <img src="@/assets/jakub2.png" alt="" />
-    </div>
-  </main>
-  <main>
-    <h1>j4mesen</h1>
-    <p>
-      is my go-to nickname on the interwebs and in games, which I play a lot of.
-      Shocker, I know, but they're the thing responsible for bringing me into
-      the world of code. I spent quite a lot of my time pushing the cart in Team
-      Fortress 2, defusing bombs in Counter-Strike or mining diamonds in
-      Minecraft, and have even tried a few times to make my own games - it
-      didn't go so well.
-    </p>
-  </main>
-  <main id="my-projects">
-    <h1>My Projects</h1>
-    <div class="cardContainer">
-      <card
-        title="/umlaut"
-        desc="A writing utility to help you use german text characters without the need of switching to a german keyboard layout."
-        meta="manczak.net/umlaut · Hosted on GitHub"
-        href="/umlaut"
-      />
-    </div>
-  </main>
-  <main id="internet-presence">
-    <h1>My Internet Presence</h1>
-    <p>
-      Places where I'm available and/or present on the Internet. Very
-      incomplete.
-    </p>
-    <div class="cardContainer">
-      <card
-        title="Steam"
-        photo="steam.png"
-        desc="Where I play games."
-        meta="manczak.net/steam"
-        href="/steam"
-      />
-      <card
-        title="GitHub"
-        photo="github.png"
-        desc="Where I do code."
-        meta="manczak.net/github"
-        href="/github"
-      />
-      <card
-        title="AniList"
-        photo="anilist.png"
-        desc="Where I note anime."
-        meta="manczak.net/anilist"
-        href="/anilist"
-      />
-      <card
-        title="Facebook"
-        photo="facebook.png"
-        desc="The Zuccening."
-        meta="manczak.net/facebook"
-        href="/facebook"
-      />
-      <card
-        title="Messenger"
-        photo="messenger.png"
-        desc="If you don't wanna go through FB."
-        meta="manczak.net/messenger"
-        href="/messenger"
-      />
-      <card
-        title="Discord"
-        photo="discord.png"
-        desc="The go-to communication app."
-        meta="j4mesen#1429"
-      />
-      <card
-        title="YouTube"
-        photo="youtube.png"
-        desc="All my shitposts for everyone to see."
-        meta="manczak.net/youtube"
-        href="/youtube"
-      />
-      <card
-        title="Twitter"
-        photo="twitter.png"
-        desc="Mostly, like, haikus and retweets."
-        meta="manczak.net/twitter"
-        href="/twitter"
-      />
-      <card
-        title="Reddit"
-        photo="reddit.png"
-        desc="The meme machine."
-        meta="manczak.net/reddit"
-        href="/reddit"
-      />
-      <card
-        title="LinkedIn"
-        photo="linkedin.png"
-        desc="The corpo correspondence."
-        meta="manczak.net/linkedin"
-        href="/linkedin"
-      />
-      <card
-        title="Secondary Steam"
-        photo="steam.png"
-        desc="My secondary steam account."
-        meta="steamcommunity.com/id/jakubmanczak2"
-      />
-      <card
-        title="Lesson Plan"
-        photo="plan-lekcji.png"
-        desc="Shortcut to my lesson plan."
-        meta="manczak.net/plan"
-        href="/plan"
-      />
-    </div>
-  </main>
-  <main id="other">
-    <h1>Other</h1>
-    <div class="cardContainer">
-      <card
-        title="/csconfig"
-        desc="My personal config files for CS:GO."
-        meta="manczak.net/csconfig · Work In Progress"
-      />
-      <card
-        title="/tfconfig"
-        desc="My personal config files for Team Fortress 2."
-        meta="manczak.net/tfconfig · Work In Progress"
-      />
-      <card
-        title="/linux-cmds"
-        desc="A list of useful linux commandline know-how with the ability to search, get more information, proper usage etc. Made to simplify Linux CMD revisions for exams."
-        meta="manczak.net/linux-cmds · Work In Progress"
-      />
-      <card
-        title="/win-cmds"
-        desc="A list of useful windows commandline know-how with the ability to search, get more information, proper usage etc. Made to simplify CMD revisions for exams."
-        meta="manczak.net/win-cmds · Work In Progress"
-      />
-    </div>
-  </main>
-  <main>
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-  </main>
+	<nav>
+		<!-- <router-link to="/">:root</router-link> -->
+		<a href="#my-projects">My Projects</a>
+		<a href="#internet-presence">My Internet Presence</a>
+		<a href="#other">Other</a>
+	</nav>
+	<main class="mainContainer">
+		<div class="mainChild short">
+			<h1>Jakub Mańczak</h1>
+			<p>
+				Hi there! 👋 <br />
+				<br />
+				My name is Jakub Mańczak and I'm a high school student learning IT from
+				near Poznań, Poland.
+			</p>
+		</div>
+		<div class="mainChild">
+			<img src="@/assets/jakub3.png" alt="" />
+			<img src="@/assets/jakub2.png" alt="" />
+		</div>
+	</main>
+	<main>
+		<h1>j4mesen</h1>
+		<p>
+			is my go-to nickname on the interwebs and in games, which I play a lot of.
+			Shocker, I know, but they're the thing responsible for bringing me into
+			the world of code. I spent quite a lot of my time pushing the cart in Team
+			Fortress 2, defusing bombs in Counter-Strike or mining diamonds in
+			Minecraft, and have even tried a few times to make my own games - it
+			didn't go so well.
+		</p>
+	</main>
+	<main id="my-projects">
+		<h1>My Projects</h1>
+		<div class="cardContainer">
+			<card
+				title="/umlaut"
+				desc="A writing utility to help you use german text characters without the need of switching to a german keyboard layout."
+				meta="manczak.net/umlaut · Hosted on GitHub"
+				href="/umlaut"
+			/>
+		</div>
+	</main>
+	<main id="internet-presence">
+		<h1>My Internet Presence</h1>
+		<p>
+			Places where I'm available and/or present on the Internet. Very
+			incomplete.
+		</p>
+		<div class="cardContainer">
+			<card
+				title="Steam"
+				photo="steam.png"
+				desc="Where I play games."
+				meta="manczak.net/steam"
+				href="/steam"
+			/>
+			<card
+				title="GitHub"
+				photo="github.png"
+				desc="Where I do code."
+				meta="manczak.net/github"
+				href="/github"
+			/>
+			<card
+				title="AniList"
+				photo="anilist.png"
+				desc="Where I note anime."
+				meta="manczak.net/anilist"
+				href="/anilist"
+			/>
+			<card
+				title="Facebook"
+				photo="facebook.png"
+				desc="The Zuccening."
+				meta="manczak.net/facebook"
+				href="/facebook"
+			/>
+			<card
+				title="Messenger"
+				photo="messenger.png"
+				desc="If you don't wanna go through FB."
+				meta="manczak.net/messenger"
+				href="/messenger"
+			/>
+			<card
+				title="Discord"
+				photo="discord.png"
+				desc="The go-to communication app."
+				meta="j4mesen#1429"
+			/>
+			<card
+				title="YouTube"
+				photo="youtube.png"
+				desc="All my shitposts for everyone to see."
+				meta="manczak.net/youtube"
+				href="/youtube"
+			/>
+			<card
+				title="Twitter"
+				photo="twitter.png"
+				desc="Mostly, like, haikus and retweets."
+				meta="manczak.net/twitter"
+				href="/twitter"
+			/>
+			<card
+				title="Reddit"
+				photo="reddit.png"
+				desc="The meme machine."
+				meta="manczak.net/reddit"
+				href="/reddit"
+			/>
+			<card
+				title="LinkedIn"
+				photo="linkedin.png"
+				desc="The corpo correspondence."
+				meta="manczak.net/linkedin"
+				href="/linkedin"
+			/>
+			<card
+				title="Secondary Steam"
+				photo="steam.png"
+				desc="My secondary steam account."
+				meta="steamcommunity.com/id/jakubmanczak2"
+			/>
+			<card
+				title="Lesson Plan"
+				photo="plan-lekcji.png"
+				desc="Shortcut to my lesson plan."
+				meta="manczak.net/plan"
+				href="/plan"
+			/>
+		</div>
+	</main>
+	<main id="other">
+		<h1>Other</h1>
+		<div class="cardContainer">
+			<card
+				title="/csconfig"
+				desc="My personal config files for CS:GO."
+				meta="manczak.net/csconfig · Work In Progress"
+			/>
+			<card
+				title="/tfconfig"
+				desc="My personal config files for Team Fortress 2."
+				meta="manczak.net/tfconfig · Work In Progress"
+			/>
+			<card
+				title="/linux-cmds"
+				desc="A list of useful linux commandline know-how with the ability to search, get more information, proper usage etc. Made to simplify Linux CMD revisions for exams."
+				meta="manczak.net/linux-cmds · Work In Progress"
+			/>
+			<card
+				title="/win-cmds"
+				desc="A list of useful windows commandline know-how with the ability to search, get more information, proper usage etc. Made to simplify CMD revisions for exams."
+				meta="manczak.net/win-cmds · Work In Progress"
+			/>
+		</div>
+	</main>
+	<main>
+		<HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+	</main>
 </template>
 
 <script lang="ts">
-  import { defineComponent } from 'vue';
-  import card from '@/components/card.vue';
+	import { defineComponent } from 'vue';
+	import card from '@/components/card.vue';
 
-  export default defineComponent({
-    name: 'Home',
-    components: {
-      card,
-    },
-  });
+	export default defineComponent({
+		name: 'Home',
+		components: {
+			card,
+		},
+	});
 </script>
 
-<style lang="sass" scoped>
-  .mainContainer
-    display: flex
-    margin-bottom: 0
-    .mainChild
-      flex: 1
-      &.short
-        h1,p
-          width: 65%
-      &:last-of-type
-        flex-grow: 1.4
-        margin-top: -2rem
-        margin-bottom: -15rem
-      img
-        border-radius: 50%
-        height: 216px
-        &:first-of-type
-          position: absolute
-          margin-left: 9rem
-          z-index: -10
-  .cardContainer
-    display: inline-flex
-    flex-wrap: wrap
-    margin: 1em 2em
-    margin-right: -12em
+<style lang="scss" scoped>
+	.mainContainer {
+		display: flex;
+		margin-bottom: 0;
+		.mainChild {
+			flex: 1;
+			&.short {
+				h1,
+				p {
+					width: 65%;
+				}
+			}
+			&:last-of-type {
+				flex-grow: 1.4;
+				margin-top: -2rem;
+				margin-bottom: -15rem;
+			}
+			img {
+				border-radius: 50%;
+				height: 216px;
+				&:first-of-type {
+					position: absolute;
+					margin-left: 9rem;
+					z-index: -10;
+				}
+			}
+		}
+	}
+	.cardContainer {
+		display: inline-flex;
+		flex-wrap: wrap;
+		margin: 1em 2em;
+		margin-right: -12em;
+	}
 
-  @media only screen and (max-width: 1100px)
-    .mainContainer
-      .mainChild
-        &:last-of-type
-          margin-top: 0rem
-    .cardContainer
-      margin: 0em 0em
-      padding: 0em 0em
-    .short
-      h1,p
-        width: 65%
-    .images
-      img
-        height: 96px
-        top: 142px
-        right: 10%
-        &:last-of-type
-          right: 28%
-  @media only screen and (max-width: 650px)
-    .mainContainer
-      flex-direction: column
-      .mainChild
-        &:last-of-type
-          margin: 0
-          order: -1
-        img
-          height: 128px
-          &:first-of-type
-            margin-left: 6rem
+	@media only screen and (max-width: 1100px) {
+		.mainContainer {
+			.mainChild {
+				&:last-of-type {
+					margin-top: 0rem;
+				}
+			}
+		}
+		.cardContainer {
+			margin: 0;
+			padding: 0;
+		}
+		.short {
+			h1,
+			p {
+				width: 65%;
+			}
+		}
+		.images {
+			img {
+				height: 96px;
+				top: 142px;
+				right: 10%;
+				&:last-of-type {
+					right: 28%;
+				}
+			}
+		}
+	}
+
+	@media only screen and (max-width: 650px) {
+		.mainContainer {
+			flex-direction: column;
+			.mainChild {
+				&:last-of-type {
+					margin: 0;
+					order: -1;
+				}
+				img {
+					height: 128px;
+					&:first-of-type {
+						margin-left: 6rem;
+					}
+				}
+			}
+		}
+	}
 </style>
