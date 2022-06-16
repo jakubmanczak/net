d6f29a6c3e33bc71948bd6282db74e501b12a370
jakubmanczak <jakub@manczak.net>
2022-06-16 19:38:21 +0200

    add basic 404page

---
+++ pages/404.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from "next";
+
+const NotFound: NextPage = () => {
+	return (
+		<>
+			<div className="constrained centertext">
+				<h1>404!</h1>
+				<p>page not found</p>
+			</div>
+		</>
+	);
+};
+
+export default NotFound;
