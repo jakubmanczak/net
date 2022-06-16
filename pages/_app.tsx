af3ae2c5b8b1b68f75aea5eb1100bc64749bf42b
jakubmanczak <jakub@manczak.net>
2022-06-16 19:24:47 +0200

    add Head with title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,14 @@
 import "/styles/master.scss";
 import type { AppProps } from "next/app";
 import Navigation from "../components/Navigation/Navigation";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
 		<>
+			<Head>
+				<title>manczak.net!</title>
+			</Head>
 			<Navigation />
 			<Component {...pageProps} />
 		</>
