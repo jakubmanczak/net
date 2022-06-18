cbc6c0144d1225b4dd76abe2338b9e3cf5703f27
jakubmanczak <jakub@manczak.net>
2022-06-18 22:29:40 +0200

    fix deploy error due to unescaped apostrophes

--- pages/info.tsx
+++ pages/info.tsx
@@ -7,14 +7,14 @@ const InfoPage: NextPage = () => {
 				<h2>Jakub Mańczak</h2>
 				<p className="mutedtext">A quick summary of who I am.</p>
 				<p className="justifytext smalltopmargin">
-					Hello! My name is Jakub Mańczak, I'm 17 and I was born in Poznań,
+					Hello! My name is Jakub Mańczak, I{"'"}m 17 and I was born in Poznań,
 					Poland. <br />
 					<br />I am about halfway done with my education in a technical school.
-					The things I'm interested in the most are what I hope to make my
+					The things I{"'"}m interested in the most are what I hope to make my
 					career one day, which is to say IT - the passion for it spawned from a
-					child's love of video games. I find the tech behind interfaces shown
-					to the user in electronic product fascinating, whether it's a website,
-					desktop software, mobile app or a videogame.
+					child{"'"}s love of video games. I find the tech behind interfaces
+					shown to the user in electronic product fascinating, whether it{"'"}s
+					a website, desktop software, mobile app or a videogame.
 				</p>
 			</div>
 			{/* <p className="smalltopmargin centertext">
@@ -23,7 +23,8 @@ const InfoPage: NextPage = () => {
 			<div className="constrained midtopmargin">
 				<h3>Aliases</h3>
 				<p className="mutedtext">
-					I've used a few names on the web in the past. Here's those I remember.
+					I{"'"}ve used a few names on the web in the past. Here{"'"}s those I
+					remember.
 				</p>
 				<p className="justifytext smalltopmargin">
 					<ul>
