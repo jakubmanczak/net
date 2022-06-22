21d492308b79dafa41bec3d9d7eb99c0a300bb9c
jakubmanczak <jakub@manczak.net>
2022-06-22 22:10:51 +0200

    change temporary splash to katakana

--- components/Profile/Profile.tsx
+++ components/Profile/Profile.tsx
@@ -12,7 +12,7 @@ const Profile: NextComponentType = () => {
 			<div className={styles.container}>
 				<div className={styles.txtside}>
 					<h2>jakub mańczak</h2>
-					<p className={styles.splash}>the installation wizard</p>
+					<p className={styles.splash}>マニチャキュ ヤクブ</p>
 					<p>
 						My name is{" "}
 						<Link href="/info">
