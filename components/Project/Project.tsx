40604b13d0e773b48d193dfff61447492ea0b2ae
jakubmanczak <jakub@manczak.net>
2022-09-26 19:00:34 +0200

    add content to projects page

---
+++ components/Project/Project.tsx
@@ -0,0 +1,29 @@
+import { InferGetStaticPropsType, NextComponentType } from "next";
+import styles from "./Project.module.scss";
+
+type ProjectProps = {
+	name?: string;
+	link?: string;
+	desc?: string;
+	date?: string;
+	classes?: string;
+};
+
+const Project = (props: ProjectProps) => {
+	return (
+		<a
+			href={props.link}
+			target="_blank"
+			className={`${props.classes} ${styles.projectcontainer}`}
+		>
+			<h1>{props.name}</h1>
+			<p className={styles.linkvisualiser}>{props.link}</p>
+			<p className={styles.description}>{props.desc}</p>
+			<div className={styles.corner}>
+				<p>{props.date}</p>
+			</div>
+		</a>
+	);
+};
+
+export default Project;
--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from "next";
+import Project from "../components/Project/Project";
 
 const ProjPage: NextPage = () => {
 	return (
@@ -9,7 +10,22 @@ const ProjPage: NextPage = () => {
 					<p className="mutedtext">
 						Index of noteworthy things I{"'"}ve done, am doing or plan to do.
 					</p>
-					<p className="mutedtext smalltopmargin">Work In Progress.</p>
+				</div>
+				<div className="projectlist">
+					<Project
+						name="PESEL"
+						link="https://numerpesel.vercel.app"
+						desc="A web app for validation and decoding of information hidden inside polish personal PESEL identifier numbers."
+						date="September 2022"
+						classes="basicAnim"
+					/>
+					<Project
+						name="Katakanize"
+						link="https://katakanize.vercel.app"
+						desc="A web app that transliterates text written in the Latin alphabet to the Japanese 'katanana' alphabet for words of foreign origin."
+						date="September 2022"
+						classes="basicAnim"
+					/>
 				</div>
 			</div>
 		</>
