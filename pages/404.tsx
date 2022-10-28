1b7972da12e2a896078107f3976fdbb73772590a
jakubmanczak <jakub@manczak.net>
2022-10-28 08:45:49 +0200

    escape the 404page apostrophe

--- pages/404.tsx
+++ pages/404.tsx
@@ -6,7 +6,7 @@ const NotFound = () => {
 			<div className="constrained centertext topmargin">
 				<h1>four hundred and four.</h1>
 				<p>
-					{document ? document.location.pathname : "this location"} doesn't
+					{document ? document.location.pathname : "this location"} doesn{"'"}t
 					exist.{" "}
 					<Link href="/">
 						<a>Return to homepage.</a>
