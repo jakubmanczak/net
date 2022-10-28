72936b5840ef282470a587328aa2f4c7c3b6a8f2
jakubmanczak <jakub@manczak.net>
2022-10-28 08:44:57 +0200

    404 page altering

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,11 +1,17 @@
-import type { NextPage } from "next";
+import Link from "next/link";
 
-const NotFound: NextPage = () => {
+const NotFound = () => {
 	return (
 		<>
-			<div className="constrained centertext">
-				<h1>404!</h1>
-				<p>page not found</p>
+			<div className="constrained centertext topmargin">
+				<h1>four hundred and four.</h1>
+				<p>
+					{document ? document.location.pathname : "this location"} doesn't
+					exist.{" "}
+					<Link href="/">
+						<a>Return to homepage.</a>
+					</Link>
+				</p>
 			</div>
 		</>
 	);
