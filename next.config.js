8d0c5c0fdcce8b99c1a7614668f6a7f6c9162295
jakubmanczak <jakub@manczak.net>
2023-01-08 22:26:58 +0100

    linking

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
+const linkscfg = require("./data/links.json");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+	reactStrictMode: true,
+	async redirects() {
+		return linkscfg
+			.filter((link) => !!link.hrefalias)
+			.map((link) =>
+				link.hrefalias.map((source) => ({
+					source,
+					destination: link.href,
+					permanent: false,
+				}))
+			)
+			.flat();
+	},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
