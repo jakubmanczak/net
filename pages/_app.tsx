af99340c4b6309aa035fa6b0618c1eac76068444
jakubmanczak <jakub@manczak.net>
2023-01-10 20:24:29 +0100

    forgor about nomotion read

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,13 @@ import "../styles/global.scss";
 import type { AppProps } from "next/app";
 import { Navigation } from "../components/Navigation/Navigation";
 import { ThemeProvider } from "next-themes";
+import { useLayoutEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
+	useLayoutEffect(() => {
+		if (window.localStorage.getItem("anims") === "false")
+			document.body.classList.add("nomotion");
+	}, []);
 	return (
 		<>
 			<ThemeProvider disableTransitionOnChange>
