fac4dc866dbdc68b1531bc72b7369af7abf5a609
jakubmanczak <jakub@manczak.net>
2023-01-10 15:27:55 +0100

    further work

--- pages/links.tsx
+++ pages/links.tsx
@@ -1,12 +1,38 @@
+import Link from "next/link";
 import { SEO } from "../components/SEO";
+import linksjson from "../data/links.json";
 
 export default function PageLinks() {
 	return (
 		<>
-			<SEO title="links & accounts" />
+			<SEO title="links" />
 			<div className="constrained basicTopMargin basicAnim">
 				<h1>/links</h1>
-				<p>This page is currently empty.</p>
+				<div
+					style={{
+						display: "flex",
+						flexWrap: "wrap",
+						gap: "16px",
+						padding: "16px 0",
+					}}
+				>
+					{linksjson.map((link) => {
+						return (
+							<Link
+								href="/"
+								className="card cardClickable"
+								style={{
+									flexGrow: 1,
+									width: 0,
+								}}
+								key={link.href}
+							>
+								<h4>{link.name}</h4>
+								<p>{link.desc}</p>
+							</Link>
+						);
+					})}
+				</div>
 			</div>
 		</>
 	);
