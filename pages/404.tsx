2f4a1246ce0603cd9b742df1c992b9040e6441ed
jakubmanczak <jakub@manczak.net>
2023-01-10 20:17:13 +0100

    fix build errors

--- pages/404.tsx
+++ pages/404.tsx
@@ -8,13 +8,15 @@ export default function PageNotFound() {
 	const [path, setPath] = useState<string>("");
 	useEffect(() => {
 		setPath(router.asPath);
-	}, [router.isReady]);
+	}, [router.isReady, router.asPath]);
 	return (
 		<>
 			<SEO title="page not found!" onlygiventitle />
 			<div className="constrained basicTopMargin basicAnim">
 				<h1>ERROR 404</h1>
-				<p>{path || "this path"} doesn't exist!</p>
+				<p>
+					{path || "this path"} {"doesn't exist!"}
+				</p>
 				<Link href="/" className="button">
 					Go back.
 				</Link>
--- pages/info.tsx
+++ pages/info.tsx
@@ -13,12 +13,12 @@ export default function PageInfo() {
 					<p style={{ textAlign: "justify" }}>
 						Hello! My name is Jakub Mańczak, I{"'"}m {getCurrentAge()} and I was
 						born in Poznań, Poland. <br /> <br />I am about halfway done with my
-						education in a technical school. The things I'm interested in the
-						most are what I hope to make my career one day, which is to say IT -
-						the passion for which spawned from a child's love of video games. I
-						find the tech behind interfaces shown to the user in electronic
-						product fascinating, whether it's a website, piece of software, app
-						or game.
+						education in a technical school. The things I{"'"}m interested in
+						the most are what I hope to make my career one day, which is to say
+						IT - the passion for which spawned from a child{"'"}s love of video
+						games. I find the tech behind interfaces shown to the user in
+						electronic product fascinating, whether it{"'"}s a website, piece of
+						software, app or game.
 					</p>
 				</main>
 			</div>
