836ef467cb1e5741295a2243c6500de9e38d5555
jakubmanczak <jakub@manczak.net>
2023-01-16 14:37:42 +0100

    projects cards fill

--- pages/links.tsx
+++ pages/links.tsx
@@ -19,23 +19,12 @@ export default function PageLinks() {
 				</p>
 			</div>
 			<div className="constrained basicAnim">
-				<div
-					style={{
-						display: "flex",
-						flexWrap: "wrap",
-						gap: "16px",
-						padding: "16px 0",
-					}}
-				>
+				<div className="cardsflex">
 					{linksjson.map((link) => {
 						return (
 							<Link
 								href={link.href}
-								className="card cardClickable"
-								style={{
-									flexGrow: 1,
-									width: 0,
-								}}
+								className="card cardClickable flexgrow"
 								key={link.href}
 							>
 								<div className="flexheader">
--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,12 +1,59 @@
+import Link from "next/link";
 import { SEO } from "../components/SEO";
+
+const projects = [
+	{
+		name: "Katakanize",
+		desc: "Convert Latin script to Katakana with ease!",
+		lang: "TypeScript",
+		dotc: "#3178c6",
+		href: "https://github.com/jakubmanczak/katakanize",
+	},
+	{
+		name: "pesel",
+		desc: "PESEL number verification and decoding.",
+		lang: "Svelte",
+		dotc: "#ff3e00",
+		href: "https://github.com/jakubmanczak/pesel",
+	},
+	{
+		name: "int-bitmap",
+		desc: "NPM package: Manipulate JavaScript numbers like a bitmask.",
+		lang: "TypeScript",
+		dotc: "#3178c6",
+		href: "https://github.com/jakubmanczak/int-bitmask",
+	},
+];
 
 export default function PageProjects() {
 	return (
 		<>
 			<SEO title="projects" />
 			<div className="constrained basicTopMargin basicAnim">
-				<h1>/projects</h1>
-				<p className="muted">This page is currently empty.</p>
+				<h1>projects / portfolio</h1>
+				<p className="muted">Things I've made I'd like to share.</p>
+			</div>
+			<div className="constrained basicAnim">
+				<div className="cardsflex">
+					{projects.map((project) => {
+						return (
+							<Link
+								key={project.href}
+								href={project.href}
+								className="card cardClickable flexgrow"
+								style={{ minWidth: "40%" }}
+							>
+								<div className="flexheader bottomSeparation">
+									<h4>{project.name}</h4>
+									<p style={{ marginLeft: "auto" }} className="muted">
+										{project.lang}
+									</p>
+								</div>
+								<p className="muted">{project.desc}</p>
+							</Link>
+						);
+					})}
+				</div>
 			</div>
 		</>
 	);
