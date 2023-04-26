b3d5f5dba402a07cd3c8171b32c70386335a2c11
jakubmanczak <jakub@manczak.net>
2023-04-27 00:14:04 +0200

    tailwind test

--- next.config.js
+++ next.config.js
@@ -1,20 +1,6 @@
-const linkscfg = require("./data/accounts.json");
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	reactStrictMode: true,
-	async redirects() {
-		return linkscfg
-			.filter((link) => !!link.hrefalias)
-			.map((link) =>
-				link.hrefalias.map((source) => ({
-					source,
-					destination: link.href,
-					permanent: false,
-				}))
-			)
-			.flat();
-	},
-};
+  reactStrictMode: true,
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+export default function PageIndex() {
+	return (
+		<>
+			<div className="rounded bg-slate-900 text-slate-300 m-4 p-4 text-center">
+				<img src="" alt="" />
+				<h1 className="text-2xl font-bold">Hello.</h1>
+				<p>This is a tailwind test. It looks fun!</p>
+			</div>
+		</>
+	);
+}
