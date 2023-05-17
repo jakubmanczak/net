e0008c92d541c2674c06593aaedb0c3a298701c9
jakubmanczak <jakub@manczak.net>
2023-05-17 12:24:24 +0200

    init rewrite

--- next.config.js
+++ next.config.js
@@ -1,20 +1,4 @@
-const linkscfg = require("./data/accounts.json");
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-	reactStrictMode: true,
-	async redirects() {
-		return linkscfg
-			.filter((link) => !!link.hrefalias)
-			.map((link) =>
-				link.hrefalias.map((source) => ({
-					source,
-					destination: link.href,
-					permanent: false,
-				}))
-			)
-			.flat();
-	},
-};
+const nextConfig = {}
 
-module.exports = nextConfig;
+module.exports = nextConfig
