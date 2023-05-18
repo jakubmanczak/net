541c2c982b40fddd8e3efbb654840984bb34ab7d
jakubmanczak <jakub@manczak.net>
2023-05-18 10:04:53 +0200

    landing & navigation & themes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,104 @@
-import Image from 'next/image'
+"use client";
+import Image from "next/image";
+import jakub from "../../public/jakub.png";
+import { useEffect, useRef, useState } from "react";
+import { IconTwitter } from "@/components/Icons/IconTwitter";
+import { IconGithub } from "@/components/Icons/IconGitHub";
 
-export default function Home() {
+export default function PageIndex() {
+  const [splash, setSplash] = useState<string>("");
+  const splashRef = useRef<HTMLParagraphElement>(null);
+
+  const getSplash = (): void => {
+    if (!splashRef.current) return;
+    let prev = splash;
+    splashRef.current.classList.remove("splashdone");
+    fetch("https://api.manczak.net/splash?personal&games")
+      .then((res) => {
+        return res.ok ? res.text() : "splashed too hard!";
+      })
+      .then((data) => {
+        if (data != prev) {
+          setSplash(data);
+          splashRef.current?.classList.add("splashdone");
+        } else {
+          getSplash();
+        }
+      })
+      .catch((err) => {
+        // console.log(err);
+        setSplash("error caught.");
+        splashRef.current?.classList.add("splashdone");
+      });
+  };
+
+  const desc =
+    "My name is Jakub Mańczak, people online call me jamesen and I'm learning IT at a High School in Poznań, Poland.";
+
+  useEffect(() => getSplash(), []);
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <>
+      <div className="max-w-3xl mx-auto pt-36 px-2 flex flex-row justify-between">
+        <div style={{ maxWidth: "50ch" }} className="text-lg flex flex-col">
+          <h1 className="font-bold text-3xl">{"jakub mańczak"}</h1>
+          <p
+            ref={splashRef}
+            className="cursor-pointer select-none opacity-0 transition-none w-full text-stone-500 dark:text-stone-400 -translate-y-5 pb-6"
+            onClick={() => getSplash()}
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            {splash || "splash not loaded"}
+          </p>
+          <p className="mt-auto">{desc}</p>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+        <Image src={jakub} alt="jakub mańczak" className="w-36 rounded-full" />
+        {/* <Image
+          src={jakub}
+          alt="jakub mańczak"
+          className="w-36 z-20 rounded-full shadow shadow-violet-200 transition-all hover:rotate-12 cursor-pointer"
         />
+        <div
+          className={`
+            relative flex flex-col w-full h-28 -ml-9 pl-14 z-10 rounded-xl my-auto
+            bg-gradient-to-r from-stone-900 to-stone-800
+            border border-stone-600 shadow p-2
+          `}
+        > */}
+        {/* <h1 className="absolute left-11 -top-9 font-bold text-4xl">
+            {"jakub mańczak"}
+          </h1> */}
+        {/* <p
+            ref={splashRef}
+            className="cursor-pointer select-none opacity-0 transition-none w-96 pb-4 text-stone-400 -translate-y-2"
+            onClick={() => getSplash()}
+          >
+            {splash || "splash not loaded"}
+          </p> */}
+        {/* <h1 className="font-bold text-3xl">jakub mańczak</h1>
+          <p style={{ maxWidth: "50ch" }} className="mt-auto">
+            {desc}
+          </p>
+          <div className="absolute top-0 right-0 p-2 gap-3 flex flex-row text-stone-500">
+            <button className="hover:text-stone-400">
+              <IconTwitter />
+            </button>
+            <button className="hover:text-stone-400">
+              <IconGithub />
+            </button>
+          </div>
+          <div className="absolute bottom-0 right-0 p-2 text-stone-500">
+            <button className="hover:underline hover:text-stone-400">
+              external link
+            </button>
+          </div>
+          <p
+            ref={splashRef}
+            className="absolute -bottom-6 left-4 cursor-pointer select-none opacity-0 transition-none text-stone-400 -translate-y-2 w-96"
+            onClick={() => getSplash()}
+          >
+            {splash || "splashed too hard."}
+          </p> */}
+        {/* </div> */}
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </>
+  );
 }
---
+++ src/components/Navigation.tsx
@@ -0,0 +1,95 @@
+"use client";
+import Link from "next/link";
+import { IconLink } from "./Icons/IconLink";
+import { IconHome } from "./Icons/IconHome";
+import { IconCPU } from "./Icons/IconCPU";
+import { useTheme } from "next-themes";
+import { IconMonitor } from "./Icons/IconMonitor";
+import { IconSun } from "./Icons/IconSun";
+import { IconMoon } from "./Icons/IconMoon";
+import { IconMeh } from "./Icons/IconMeh";
+import { useEffect, useState } from "react";
+
+const Navigation = () => {
+  const navlink = `
+		p-2 rounded flex flex-row gap-2 border
+		first-of-type:rounded-tl-full first-of-type:rounded-bl-full
+		last-of-type:rounded-tr-full last-of-type:rounded-br-full
+
+		border-stone-300 bg-stone-100
+		hover:border-stone-400 hover:bg-stone-200
+		dark:border-stone-600 dark:bg-stone-800
+		dark:hover:border-stone-500 dark:hover:bg-stone-700
+	`;
+  const themebtn = `
+		p-2 rounded-full border ml-auto
+		
+		border-stone-300 bg-stone-100
+		hover:border-stone-400 hover:bg-stone-200
+		dark:border-stone-600 dark:bg-stone-800
+		dark:hover:border-stone-500 dark:hover:bg-stone-700
+	`;
+
+  const { theme, resolvedTheme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState<boolean>(false);
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  return (
+    <>
+      <div className="max-w-3xl mx-auto px-2 py-4 flex flex-row flex-grow gap-2">
+        <Link className={navlink} href="/">
+          <div className="scale-75">
+            <IconHome />
+          </div>
+          {"home"}
+        </Link>
+        <Link className={navlink} href="/">
+          <div className="scale-75">
+            <IconCPU />
+          </div>
+          {"projects"}
+        </Link>
+        <Link className={navlink} href="/">
+          <div className="scale-75">
+            <IconLink />
+          </div>
+          {"links"}
+        </Link>
+        <button
+          className={themebtn}
+          onClick={() => {
+            if (theme === "system") setTheme("light");
+            else if (theme === "light") setTheme("dark");
+            else if (theme === "dark") setTheme("system");
+            else setTheme("system");
+          }}
+        >
+          <div className="scale-75">
+            {mounted ? (
+              theme === "light" ? (
+                <IconSun />
+              ) : theme === "dark" ? (
+                <IconMoon />
+              ) : theme === "system" ? (
+                <div className="relative">
+                  <IconMonitor />
+                  {/* <div className="absolute scale-50 -top-[2px] left-0"> */}
+                  {/* {resolvedTheme === "dark" ? <IconMoon /> : <IconSun />} */}
+                  {/* </div> */}
+                </div>
+              ) : (
+                <IconMeh />
+              )
+            ) : (
+              <div className="w-6 h-6 rounded-lg bg-stone-500 animate-pulse"></div>
+            )}
+          </div>
+        </button>
+      </div>
+    </>
+  );
+};
+
+export { Navigation };
