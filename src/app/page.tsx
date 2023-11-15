f340f71be360b97a8408f38756250749682ed083
jakubmanczak <jakub@manczak.net>
2023-11-15 11:32:24 +0100

    projects page & index page misc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,11 @@
 import { Profile } from "@/components/Profile";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <>
       <div className="mt-8 lg:mt-24">
         <Profile />
-        <hr className="border-neutral-700 mt-0 lg:mt-8" />
-        <section>
-          {/* <p>This is me!</p> */}
-          {/* <Image /> */}
-        </section>
       </div>
     </>
   );
---
+++ src/app/projects/page.tsx
@@ -0,0 +1,9 @@
+export default function Projectspage() {
+  return (
+    <>
+      <div className="mt-8 lg:mt-24">
+        <p>Projects will be loaded here.</p>
+      </div>
+    </>
+  );
+}
