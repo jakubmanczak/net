e4d78c9395b9be05d526bfbb6d70bdf92e430430
jakubmanczak <jakub@manczak.net>
2023-11-15 11:40:57 +0100

    quick /projects misc

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,8 +1,17 @@
+import { IconCPU } from "@/components/icons/IconCPU";
+import { Lexend } from "next/font/google";
+
+const lexend = Lexend({ subsets: ["latin"] });
+
 export default function Projectspage() {
   return (
     <>
-      <div className="mt-8 lg:mt-24">
-        <p>Projects will be loaded here.</p>
+      <div className="mt-8 lg:mt-24 max-w-4xl w-full">
+        <h1 className={`${lexend.className} flex flex-row gap-2 mb-2`}>
+          <IconCPU />
+          projects
+        </h1>
+        <p className="text-neutral-500">Projects will be shown here.</p>
       </div>
     </>
   );
