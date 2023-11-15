1cf4eec930fc4e6bbad408351ce2704b53d1b5a4
jakubmanczak <jakub@manczak.net>
2023-11-15 15:37:01 +0100

    projects page finalize functionality

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,7 +1,43 @@
 import { IconCPU } from "@/components/icons/IconCPU";
 import { Lexend } from "next/font/google";
+import Link from "next/link";
 
 const lexend = Lexend({ subsets: ["latin"] });
+
+type projectlang = "C" | "React" | "Svelte";
+type project = {
+  name: string;
+  desc?: string;
+  lang?: projectlang;
+  href?: string;
+  repo?: string;
+  styles?: string;
+};
+
+const projects: project[] = [
+  {
+    name: "Debate Tools",
+    desc: "Utilities to aid in conducting of oxford format debates.",
+    href: "https://debates.manczak.net/",
+    repo: "https://github.com/jakubmanczak/debates",
+    lang: "React",
+    styles: "sm:col-span-2",
+  },
+  {
+    name: "Katakanize",
+    desc: "Latin to Katakana transliteration.",
+    lang: "Svelte",
+    href: "https://katakanize.vercel.app",
+    repo: "https://github.com/jakubmanczak/katakanize",
+  },
+  {
+    name: "PESEL",
+    desc: "Verification and decoding of pesel numbers.",
+    lang: "Svelte",
+    href: "https://numerpesel.vercel.app",
+    repo: "https://github.com/jakubmanczak/pesel",
+  },
+];
 
 export default function Projectspage() {
   return (
@@ -14,6 +50,54 @@ export default function Projectspage() {
         <p className="text-neutral-500 mb-8">
           {"Things I've made I'm proud of."}
         </p>
+        <div className="mt-4 grid grid-cols-1 sm:grid-cols-2 gap-4">
+          {projects.map((el: project) => {
+            return (
+              <div
+                className={`rounded bg-neutral-800 border-neutral-700 border-[1px] flex flex-col ${el.styles}`}
+                key={`${el.name}${el.href}${el.desc}`}
+              >
+                <div className="p-4">
+                  <p className={lexend.className}>{el.name}</p>
+                  <p className="text-neutral-400">{el.desc}</p>
+                </div>
+                {el.href || el.repo ? (
+                  <hr className="border-neutral-700 mt-auto" />
+                ) : (
+                  ""
+                )}
+                <div className="flex flex-row text-center divide-solid divide-x-[1px] divide-neutral-700">
+                  {el.href ? (
+                    <Link
+                      href={el.href}
+                      target="_blank"
+                      className={`w-full p-2 transition ${
+                        el.repo ? "rounded-bl" : "rounded-b"
+                      } hover:bg-neutral-900`}
+                    >
+                      <p>Project link</p>
+                    </Link>
+                  ) : (
+                    ""
+                  )}
+                  {el.repo ? (
+                    <Link
+                      href={el.repo}
+                      target="_blank"
+                      className={`w-full p-2 transition ${
+                        el.href ? "rounded-br" : "rounded-b"
+                      } hover:bg-neutral-900`}
+                    >
+                      <p>Source code</p>
+                    </Link>
+                  ) : (
+                    ""
+                  )}
+                </div>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </>
   );
