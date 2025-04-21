175e87ff289661882fccee67c1c614860fe409a2
jakubmanczak <jakub@manczak.net>
2025-04-21 21:07:31 +0200

    basic source server query endpoint

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -1,10 +1,23 @@
-use axum::{Router, routing::get};
+use axum::{
+    Router,
+    http::StatusCode,
+    response::{IntoResponse, Response},
+    routing::get,
+};
+use sourcequery::source_query;
 use splash::{splash, splashes};
 
+pub mod sourcequery;
 pub mod splash;
 
 pub fn router() -> Router {
     Router::new()
+        .route("/teapot", get(teapot))
         .route("/splash", get(splash))
         .route("/splashes", get(splashes))
+        .route("/server-query/source/{socket}", get(source_query))
+}
+
+async fn teapot() -> Response {
+    StatusCode::IM_A_TEAPOT.into_response()
 }
---
+++ src/api/sourcequery.rs
@@ -0,0 +1,77 @@
+use a2s::{A2SClient, info::ServerOS};
+use axum::{
+    Json,
+    extract::Path,
+    http::StatusCode,
+    response::{IntoResponse, Response},
+};
+use serde::Serialize;
+
+pub async fn source_query(Path(socket): Path<String>) -> Result<Response, SourceQueryError> {
+    let cl = A2SClient::new()?;
+    // let socket = format!("{addr}:{port}");
+    let info = cl.info(&socket)?;
+    let players: Vec<SourceQueryPlayer> = cl
+        .players(&socket)?
+        .into_iter()
+        .map(|p| SourceQueryPlayer {
+            name: p.name,
+            score: p.score,
+            duration: p.duration,
+        })
+        .collect();
+    let server = SourceQueryInfo {
+        hostname: info.name,
+        map: info.map,
+        server_os: match info.server_os {
+            ServerOS::Windows => "Windows",
+            ServerOS::Linux => "Linux",
+            ServerOS::Mac => "Mac",
+        }
+        .to_string(),
+        game_version: info.version,
+        maxplayers: info.max_players,
+        players: info.players,
+        bots: info.bots,
+        vac: info.vac,
+        passworded: info.visibility,
+        playerlist: players,
+    };
+    Ok(Json(server).into_response())
+}
+
+#[derive(Serialize)]
+struct SourceQueryInfo {
+    hostname: String,
+    map: String,
+    server_os: String,
+    game_version: String,
+    maxplayers: u8,
+    players: u8,
+    bots: u8,
+    vac: bool,
+    passworded: bool,
+    playerlist: Vec<SourceQueryPlayer>,
+}
+
+#[derive(Serialize)]
+struct SourceQueryPlayer {
+    name: String,
+    score: i32,
+    duration: f32,
+}
+
+pub struct SourceQueryError(a2s::errors::Error);
+impl From<a2s::errors::Error> for SourceQueryError {
+    fn from(value: a2s::errors::Error) -> Self {
+        Self(value)
+    }
+}
+
+impl IntoResponse for SourceQueryError {
+    fn into_response(self) -> Response {
+        match self.0 {
+            _ => (StatusCode::BAD_REQUEST, self.0.to_string()).into_response(),
+        }
+    }
+}
