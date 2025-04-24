1da9322054881d4c0cbe07822812ddaa732276a0
jakubmanczak <jakub@manczak.net>
2025-04-24 17:34:59 +0200

    sourcequery implementation semantics

--- src/api/sourcequery.rs
+++ src/api/sourcequery.rs
@@ -10,32 +10,8 @@ use serde::Serialize;
 pub async fn source_query(Path(socket): Path<String>) -> Result<Response, SourceQueryError> {
     let cl = A2SClient::new()?;
     let info = cl.info(&socket)?;
-    let players: Vec<SourceQueryPlayer> = cl
-        .players(&socket)?
-        .into_iter()
-        .map(|p| SourceQueryPlayer {
-            name: p.name,
-            score: p.score,
-            duration: p.duration,
-        })
-        .collect();
-    let server = SourceQueryInfo {
-        hostname: info.name,
-        map: info.map,
-        server_os: match info.server_os {
-            ServerOS::Windows => "Windows",
-            ServerOS::Linux => "Linux",
-            ServerOS::Mac => "Mac",
-        }
-        .to_string(),
-        game_version: info.version,
-        maxplayers: info.max_players,
-        players: info.players,
-        bots: info.bots,
-        vac: info.vac,
-        passworded: info.visibility,
-        playerlist: players,
-    };
+    let players = cl.players(&socket)?;
+    let server = SourceQueryInfo::from((info, players));
     Ok(Json(server).into_response())
 }
 
@@ -58,6 +34,35 @@ struct SourceQueryPlayer {
     name: String,
     score: i32,
     duration: f32,
+}
+
+impl From<(a2s::info::Info, Vec<a2s::players::Player>)> for SourceQueryInfo {
+    fn from((info, players): (a2s::info::Info, Vec<a2s::players::Player>)) -> Self {
+        Self {
+            hostname: info.name,
+            map: info.map,
+            server_os: match info.server_os {
+                ServerOS::Windows => "Windows",
+                ServerOS::Linux => "Linux",
+                ServerOS::Mac => "Mac",
+            }
+            .to_string(),
+            game_version: info.version,
+            maxplayers: info.max_players,
+            players: info.players,
+            bots: info.bots,
+            vac: info.vac,
+            passworded: info.visibility,
+            playerlist: players
+                .into_iter()
+                .map(|p| SourceQueryPlayer {
+                    name: p.name,
+                    score: p.score,
+                    duration: p.duration,
+                })
+                .collect(),
+        }
+    }
 }
 
 pub struct SourceQueryError(a2s::errors::Error);
