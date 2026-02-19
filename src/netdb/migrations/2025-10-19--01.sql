0f37861c7e69d206f19dfb6cd8dfcd3244bccad4
jakubmanczak <jakub@manczak.net>
2026-02-19 15:12:32 +0100

    remove old migrations

--- src/netdb/migrations/2025-10-19--01.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE links (
-    id              TEXT NOT NULL UNIQUE PRIMARY KEY,
-    destination     TEXT NOT NULL,
-    revoked         INTEGER DEFAULT 0,
-    created_at      INTEGER NOT NULL
-);
--- src/netdb/migrations/2025-11-13--01.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE splashes (
-    id              TEXT NOT NULL UNIQUE PRIMARY KEY,
-    content         TEXT NOT NULL UNIQUE
-);
